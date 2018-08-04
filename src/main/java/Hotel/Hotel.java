34775e57202a392c8915077a932b523ddbd8661e
Pat <graczyk.pat@gmail.com>
2018-08-04 17:35:32 +0100

    dinning room added

--- src/main/java/Hotel/Hotel.java
+++ src/main/java/Hotel/Hotel.java
@@ -3,6 +3,7 @@ package Hotel;
 import People.Guest;
 import Rooms.Bedroom;
 import Rooms.ConferenceRoom;
+import Rooms.DiningRoom;
 import Rooms.Room;
 
 import java.util.ArrayList;
@@ -10,10 +11,12 @@ import java.util.ArrayList;
 public class Hotel {
     private ArrayList<Bedroom> bedrooms;
     private ArrayList<ConferenceRoom> conferenceRooms;
+    private DiningRoom diningRoom;
 
     public Hotel() {
         this.bedrooms = bedrooms;
         this.conferenceRooms=conferenceRooms;
+        this.diningRoom= diningRoom;
     }
 
     public ArrayList<Bedroom> getBedrooms() {
--- src/main/java/Rooms/DiningRoom.java
+++ src/main/java/Rooms/DiningRoom.java
@@ -1,4 +1,21 @@
 package Rooms;
 
-public class DiningRoom {
+public class DiningRoom extends Room {
+
+    private String dishOfTheDay;
+
+    public DiningRoom (String getDishOfTheDay, int capacity, int rate){
+        super(capacity, rate);
+        this.dishOfTheDay=dishOfTheDay;
+    }
+
+    public String getDishOfTheDay(){
+        return this.dishOfTheDay;
+    }
+
+    @Override
+    public String roomInformation() {
+        return "This is your dining area. Today's meal of the day is: " + dishOfTheDay;
+    }
+
 }
